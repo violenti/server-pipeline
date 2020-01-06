616a848a0baafeba37325de64be72779df0ccef5
david fernandez <rodolfo.fernandez@fravega.com.ar>
2020-01-06 14:40:26 -0300

    add development

--- main.go
+++ main.go
@@ -10,13 +10,13 @@ import (
     "github.com/gorilla/mux"
 )
 
-//var GITLAB_TOKEN = os.Getenv("TOKEN")
-//var URI = os.Getenv("GITLAB")
-//var FULL_URI= URI + GITLAB_TOKEN
+var GITLAB_TOKEN= os.Getenv("GITLAB_TOKEN")
+var URI = os.Getenv("GITLAB")
 
 func main ()  {
   r := mux.NewRouter()
   r.HandleFunc("/master/{file}", Master)
+  r.HandleFunc("/development/{file}", Development)
   http.Handle("/", r)
 
   srv := &http.Server{
@@ -33,11 +33,34 @@ func main ()  {
 
 func Master (w http.ResponseWriter, r *http.Request)  {
 
-  var GITLAB_TOKEN= os.Getenv("GITLAB_TOKEN")
-  var URI = os.Getenv("GITLAB")
   var varrequest = mux.Vars(r)
   var FILE = varrequest["file"]
   var FULL_URI= URI + FILE + "/raw?ref=master" + "&private_token=" + GITLAB_TOKEN
+
+  resp, err:= http.Get(FULL_URI)
+  if err != nil {
+    log.Fatalln(err)
+    fmt.Printf("%s", err)
+    os.Exit(1)
+  } else {
+    defer resp.Body.Close()
+    contents, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        log.Fatalln(err)
+        os.Exit(1)
+    }
+    fmt.Fprint(w,string(contents))
+    fmt.Print(FULL_URI)
+
+  }
+}
+
+func Development (w http.ResponseWriter, r *http.Request)  {
+
+
+  var varrequest = mux.Vars(r)
+  var FILE = varrequest["file"]
+  var FULL_URI= URI + FILE + "/raw?ref=develop" + "&private_token=" + GITLAB_TOKEN
 
   resp, err:= http.Get(FULL_URI)
   if err != nil {
