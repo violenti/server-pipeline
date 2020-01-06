b28e6fcb6cf674d3bf8f5250918e3676d68e1a0c
david fernandez <rodolfo.fernandez@fravega.com.ar>
2020-01-06 14:25:34 -0300

    add ux env vars

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Proyecto para no exponer token de gitlab
 ## URL  modelo
 
 https://gitlab.com/api/v4/projects/9927708/repository/files/default-ci.yaml?ref=master&private_token=mZg.....&.yaml
+
+##
+
+export GITLAB_TOKEN=djkfjdkfjdkjd
+export GITLAB=https://gitlab.com/api/v4/projects/9927708/repository/files/
+export 
--- main.go
+++ main.go
@@ -10,16 +10,17 @@ import (
     "github.com/gorilla/mux"
 )
 
-var GITLAB_TOKEN = os.Getenv("TOKEN")
-var URI = os.Getenv("GITLAB")
-var FULL_URI= URI + GITLAB_TOKEN
+//var GITLAB_TOKEN = os.Getenv("TOKEN")
+//var URI = os.Getenv("GITLAB")
+//var FULL_URI= URI + GITLAB_TOKEN
 
 func main ()  {
   r := mux.NewRouter()
-  r.HandleFunc("/master/default.yml", Master)
+  r.HandleFunc("/master/{file}", Master)
+  http.Handle("/", r)
 
   srv := &http.Server{
-        Addr:         "0.0.0.0:8080",
+        Addr: "127.0.0.1:8000",
         // Good practice to set timeouts to avoid Slowloris attacks.
         WriteTimeout: time.Second * 15,
         ReadTimeout:  time.Second * 15,
@@ -29,7 +30,15 @@ func main ()  {
     log.Fatal(srv.ListenAndServe())
 }
 
+
 func Master (w http.ResponseWriter, r *http.Request)  {
+
+  var GITLAB_TOKEN= os.Getenv("GITLAB_TOKEN")
+  var URI = os.Getenv("GITLAB")
+  var varrequest = mux.Vars(r)
+  var FILE = varrequest["file"]
+  var FULL_URI= URI + FILE + "/raw?ref=master" + "&private_token=" + GITLAB_TOKEN
+
   resp, err:= http.Get(FULL_URI)
   if err != nil {
     log.Fatalln(err)
