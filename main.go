d08f532894977201f256d33b6ef4fdc262eafa42
david <rodolfo.fernandez@fravega.com.ar>
2021-08-22 18:41:52 -0300

    add type method

--- main.go
+++ main.go
@@ -1,82 +1,81 @@
 package main
 
 import (
-    "io/ioutil"
-    "time"
-    "log"
-    "fmt"
-    "net/http"
-    "os"
-    "github.com/gorilla/mux"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
-var GITLAB_TOKEN= os.Getenv("GITLAB_TOKEN")
+var GITLAB_TOKEN = os.Getenv("GITLAB_TOKEN")
 var URI = os.Getenv("GITLAB")
 
-func main ()  {
-  r := mux.NewRouter()
-  r.HandleFunc("/master/{file}", Master)
-  r.HandleFunc("/development/{file}", Development)
-  http.Handle("/", r)
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/master/{file}", Master).Methods("GET")
+	r.HandleFunc("/development/{file}", Development).Methods("GET")
+	http.Handle("/", r)
 
-  srv := &http.Server{
-        Addr: "0.0.0.0:8000",
-        // Good practice to set timeouts to avoid Slowloris attacks.
-        WriteTimeout: time.Second * 15,
-        ReadTimeout:  time.Second * 15,
-        IdleTimeout:  time.Second * 60,
-        Handler: r, // Pass our instance of gorilla/mux in.
-    }
-    fmt.Println("server-pipeline running on http://0.0.0.0:8000 ")
-    log.Fatal(srv.ListenAndServe() )
+	srv := &http.Server{
+		Addr: "0.0.0.0:8000",
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      r, // Pass our instance of gorilla/mux in.
+	}
+	fmt.Println("server-pipeline running on http://0.0.0.0:8000 ")
+	srv.ListenAndServe()
 }
 
+func Master(w http.ResponseWriter, r *http.Request) {
 
-func Master (w http.ResponseWriter, r *http.Request)  {
+	var varrequest = mux.Vars(r)
+	var FILE = varrequest["file"]
+	var FULL_URI = URI + FILE + "/raw?ref=master" + "&private_token=" + GITLAB_TOKEN
 
-  var varrequest = mux.Vars(r)
-  var FILE = varrequest["file"]
-  var FULL_URI= URI + FILE + "/raw?ref=master" + "&private_token=" + GITLAB_TOKEN
+	resp, err := http.Get(FULL_URI)
+	if err != nil {
+		log.Fatalln(err)
+		fmt.Printf("%s", err)
+		os.Exit(1)
+	} else {
+		defer resp.Body.Close()
+		contents, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+			os.Exit(1)
+		}
+		fmt.Fprint(w, string(contents))
+		fmt.Print(FULL_URI)
 
-  resp, err:= http.Get(FULL_URI)
-  if err != nil {
-    log.Fatalln(err)
-    fmt.Printf("%s", err)
-    os.Exit(1)
-  } else {
-    defer resp.Body.Close()
-    contents, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        log.Fatalln(err)
-        os.Exit(1)
-    }
-    fmt.Fprint(w,string(contents))
-    fmt.Print(FULL_URI)
-
-  }
+	}
 }
 
-func Development (w http.ResponseWriter, r *http.Request)  {
+func Development(w http.ResponseWriter, r *http.Request) {
 
+	var varrequest = mux.Vars(r)
+	var FILE = varrequest["file"]
+	var FULL_URI = URI + FILE + "/raw?ref=develop" + "&private_token=" + GITLAB_TOKEN
 
-  var varrequest = mux.Vars(r)
-  var FILE = varrequest["file"]
-  var FULL_URI= URI + FILE + "/raw?ref=develop" + "&private_token=" + GITLAB_TOKEN
+	resp, err := http.Get(FULL_URI)
+	if err != nil {
+		log.Fatalln(err)
+		fmt.Printf("%s", err)
+		os.Exit(1)
+	} else {
+		defer resp.Body.Close()
+		contents, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+			os.Exit(1)
+		}
+		fmt.Fprint(w, string(contents))
+		fmt.Print(FULL_URI)
 
-  resp, err:= http.Get(FULL_URI)
-  if err != nil {
-    log.Fatalln(err)
-    fmt.Printf("%s", err)
-    os.Exit(1)
-  } else {
-    defer resp.Body.Close()
-    contents, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        log.Fatalln(err)
-        os.Exit(1)
-    }
-    fmt.Fprint(w,string(contents))
-    fmt.Print(FULL_URI)
-
-  }
+	}
 }
